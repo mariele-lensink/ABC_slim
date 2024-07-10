62c5d23b4c6f39fe65d3983ac0a5b80e033efd73
mariele-lensink <marielelensink325@gmail.com>
2024-07-10 15:00:15 -0700

    removing int() command

--- scripts/generate_parameters.R
+++ scripts/generate_parameters.R
@@ -7,7 +7,7 @@ set.seed(as.integer(Sys.time()))
 # Generate 100,000 unique IDs and random parameters
 num_simulations <- 500000
 params <- data.table(
-  ID = int(1:num_simulations),
+  ID = 1:num_simulations,
   gmu = runif(num_simulations,2.6e-8,5.3e-6),
   imu = runif(num_simulations,2.6e-8,5.3e-6),
   gd = runif(num_simulations,0.1,0.7),
