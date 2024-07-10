78e5c2627e659a0ce22ada6405f10be07f2439b5
mariele-lensink <marielelensink325@gmail.com>
2024-07-10 13:56:47 -0700

    forcing integer

--- scripts/generate_parameters.R
+++ scripts/generate_parameters.R
@@ -7,7 +7,7 @@ set.seed(as.integer(Sys.time()))
 # Generate 100,000 unique IDs and random parameters
 num_simulations <- 500000
 params <- data.table(
-  ID = num_simulations:num_simulations+500000,
+  ID = int(num_simulations:num_simulations+500000),
   gmu = runif(num_simulations,1e-10,2e-8),
   imu = runif(num_simulations,1e-10,2e-8),
   gd = runif(num_simulations,0.1,0.7),
