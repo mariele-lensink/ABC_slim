fb27290e05fad37ea0c9f683c15cd341bbb2f054
mariele-lensink <marielelensink325@gmail.com>
2025-05-14 16:17:36 -0700

    new run to 50k

--- scripts/generate_parameters.R
+++ scripts/generate_parameters.R
@@ -6,12 +6,12 @@ library(parallel)
 set.seed(as.integer(Sys.time()))
 
 # Generate 100,000 unique IDs and random parameters
-num_simulations <- 10000
+num_simulations <- 50000
 log_uniform <- function(n, min, max) 10^runif(n, log10(min), log10(max))
 log_uniform_neg <- function(n, min_abs, max_abs) -10^runif(n, log10(min_abs), log10(max_abs))
 
 params <- data.table(
-  ID = 10001:(10001 + num_simulations - 1),
+  ID = 20001:(20001 + num_simulations - 1),
   gmu = log_uniform(num_simulations, 3e-8, 5.3e-5),
   imu = log_uniform(num_simulations, 3e-8, 5.3e-6),
   gd = runif(num_simulations,0.1,0.7),
@@ -23,4 +23,4 @@ params <- data.table(
 )
 
 # Write to a CSV file
-fwrite(params, "/home/mlensink/slimsimulations/ABCslim/ABC_slim/data/prior_parameters2_failtest.csv", sep = ",", col.names = TRUE)
+fwrite(params, "/home/mlensink/slimsimulations/ABCslim/ABC_slim/data/prior_parameters3_20k-50k.csv", sep = ",", col.names = TRUE)
