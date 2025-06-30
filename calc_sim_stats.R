2e874b7de77c2094a00327d943b52cc5a5a6762f
mariele-lensink <marielelensink325@gmail.com>
2025-06-30 14:08:03 -0700

    34k sims to add

--- calc_sim_stats.R
+++ calc_sim_stats.R
@@ -56,5 +56,5 @@ results <- Filter(Negate(is.null), results)  # remove NULLs before binding
 
 # Combine all results into one data.table
 final_stats <- rbindlist(results)
-fwrite(final_stats, "sim_tajima_stats_6.25.2025.csv")
+fwrite(final_stats, "sim_tajima_stats_6.30.2025.full.csv")
 
